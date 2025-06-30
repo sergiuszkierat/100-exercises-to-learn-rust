e175ad32f0da002fcb8bba45ebcb5ff61187e174
Sergiusz Kierat <sergiusz.kierat@gmail.com>
2025-06-30 07:33:23 +0200

    12_two_states

--- exercises/06_ticket_management/12_two_states/src/lib.rs
+++ exercises/06_ticket_management/12_two_states/src/lib.rs
@@ -44,8 +44,20 @@ impl TicketStore {
         }
     }
 
-    pub fn add_ticket(&mut self, ticket: Ticket) {
+    pub fn add_ticket(&mut self, ticket_draft: TicketDraft) -> TicketId {
+        let id = TicketId(self.tickets.len() as u64 + 1); // Simple ID generation
+        let ticket = Ticket {
+            id,
+            title: ticket_draft.title,
+            description: ticket_draft.description,
+            status: Status::ToDo,
+        };
         self.tickets.push(ticket);
+        id
+    }
+    
+    pub fn get(&self, id: TicketId) -> Option<&Ticket> {
+        self.tickets.iter().find(|ticket| ticket.id == id)
     }
 }
 
